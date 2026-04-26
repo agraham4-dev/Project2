768eeef78059d927f086b29d2e86ad973edda623
agraham4-dev <agraham4@umbc.edu>
2026-04-26 15:28:47 -0400

    Add files via upload

---
+++ proj2.s
@@ -0,0 +1,115 @@
+	.file	"proj2.cpp"
+	.text
+	.local	_ZStL8__ioinit
+	.comm	_ZStL8__ioinit,1,1
+	.section	.rodata
+.LC0:
+	.string	"the double is:"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1723:
+	.cfi_startproc
+	leal	4(%esp), %ecx
+	.cfi_def_cfa 1, 0
+	andl	$-16, %esp
+	pushl	-4(%ecx)
+	pushl	%ebp
+	movl	%esp, %ebp
+	.cfi_escape 0x10,0x5,0x2,0x75,0
+	pushl	%ecx
+	.cfi_escape 0xf,0x3,0x75,0x7c,0x6
+	subl	$20, %esp
+	subl	$8, %esp
+	leal	-12(%ebp), %eax
+	pushl	%eax
+	pushl	$_ZSt3cin
+	call	_ZNSirsERi
+	addl	$16, %esp
+	movl	-12(%ebp), %eax
+	addl	%eax, %eax
+	movl	%eax, -12(%ebp)
+	subl	$8, %esp
+	pushl	$.LC0
+	pushl	$_ZSt4cout
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	addl	$16, %esp
+	movl	-12(%ebp), %edx
+	subl	$8, %esp
+	pushl	%edx
+	pushl	%eax
+	call	_ZNSolsEi
+	addl	$16, %esp
+	movl	$0, %eax
+	movl	-4(%ebp), %ecx
+	.cfi_def_cfa 1, 0
+	leave
+	.cfi_restore 5
+	leal	-4(%ecx), %esp
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE1723:
+	.size	main, .-main
+	.type	_Z41__static_initialization_and_destruction_0ii, @function
+_Z41__static_initialization_and_destruction_0ii:
+.LFB2224:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$8, %esp
+	cmpl	$1, 8(%ebp)
+	jne	.L5
+	cmpl	$65535, 12(%ebp)
+	jne	.L5
+	subl	$12, %esp
+	pushl	$_ZStL8__ioinit
+	call	_ZNSt8ios_base4InitC1Ev
+	addl	$16, %esp
+	subl	$4, %esp
+	pushl	$__dso_handle
+	pushl	$_ZStL8__ioinit
+	pushl	$_ZNSt8ios_base4InitD1Ev
+	call	__cxa_atexit
+	addl	$16, %esp
+.L5:
+	nop
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE2224:
+	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
+	.type	_GLOBAL__sub_I_main, @function
+_GLOBAL__sub_I_main:
+.LFB2225:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$8, %esp
+	subl	$8, %esp
+	pushl	$65535
+	pushl	$1
+	call	_Z41__static_initialization_and_destruction_0ii
+	addl	$16, %esp
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE2225:
+	.size	_GLOBAL__sub_I_main, .-_GLOBAL__sub_I_main
+	.section	.init_array,"aw"
+	.align 4
+	.long	_GLOBAL__sub_I_main
+	.hidden	__dso_handle
+	.ident	"GCC: (GNU) 11.5.0 20240719 (Red Hat 11.5.0-5)"
+	.section	.note.GNU-stack,"",@progbits
