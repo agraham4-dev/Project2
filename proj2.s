959dc9141e403d9511d6d1a38b72f259737b03d3
agraham4-dev <agraham4@umbc.edu>
2026-04-29 20:39:02 -0400

    Refactor proj2.s to implement new functionality

--- proj2.s
+++ proj2.s
@@ -1,115 +1,76 @@
-	.file	"proj2.cpp"
-	.text
-	.local	_ZStL8__ioinit
-	.comm	_ZStL8__ioinit,1,1
-	.section	.rodata
-.LC0:
-	.string	"the double is:"
-	.text
-	.globl	main
-	.type	main, @function
-main:
-.LFB1723:
-	.cfi_startproc
-	leal	4(%esp), %ecx
-	.cfi_def_cfa 1, 0
-	andl	$-16, %esp
-	pushl	-4(%ecx)
-	pushl	%ebp
-	movl	%esp, %ebp
-	.cfi_escape 0x10,0x5,0x2,0x75,0
-	pushl	%ecx
-	.cfi_escape 0xf,0x3,0x75,0x7c,0x6
-	subl	$20, %esp
-	subl	$8, %esp
-	leal	-12(%ebp), %eax
-	pushl	%eax
-	pushl	$_ZSt3cin
-	call	_ZNSirsERi
-	addl	$16, %esp
-	movl	-12(%ebp), %eax
-	addl	%eax, %eax
-	movl	%eax, -12(%ebp)
-	subl	$8, %esp
-	pushl	$.LC0
-	pushl	$_ZSt4cout
-	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
-	addl	$16, %esp
-	movl	-12(%ebp), %edx
-	subl	$8, %esp
-	pushl	%edx
-	pushl	%eax
-	call	_ZNSolsEi
-	addl	$16, %esp
-	movl	$0, %eax
-	movl	-4(%ebp), %ecx
-	.cfi_def_cfa 1, 0
-	leave
-	.cfi_restore 5
-	leal	-4(%ecx), %esp
-	.cfi_def_cfa 4, 4
-	ret
-	.cfi_endproc
-.LFE1723:
-	.size	main, .-main
-	.type	_Z41__static_initialization_and_destruction_0ii, @function
-_Z41__static_initialization_and_destruction_0ii:
-.LFB2224:
-	.cfi_startproc
-	pushl	%ebp
-	.cfi_def_cfa_offset 8
-	.cfi_offset 5, -8
-	movl	%esp, %ebp
-	.cfi_def_cfa_register 5
-	subl	$8, %esp
-	cmpl	$1, 8(%ebp)
-	jne	.L5
-	cmpl	$65535, 12(%ebp)
-	jne	.L5
-	subl	$12, %esp
-	pushl	$_ZStL8__ioinit
-	call	_ZNSt8ios_base4InitC1Ev
-	addl	$16, %esp
-	subl	$4, %esp
-	pushl	$__dso_handle
-	pushl	$_ZStL8__ioinit
-	pushl	$_ZNSt8ios_base4InitD1Ev
-	call	__cxa_atexit
-	addl	$16, %esp
-.L5:
-	nop
-	leave
-	.cfi_restore 5
-	.cfi_def_cfa 4, 4
-	ret
-	.cfi_endproc
-.LFE2224:
-	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
-	.type	_GLOBAL__sub_I_main, @function
-_GLOBAL__sub_I_main:
-.LFB2225:
-	.cfi_startproc
-	pushl	%ebp
-	.cfi_def_cfa_offset 8
-	.cfi_offset 5, -8
-	movl	%esp, %ebp
-	.cfi_def_cfa_register 5
-	subl	$8, %esp
-	subl	$8, %esp
-	pushl	$65535
-	pushl	$1
-	call	_Z41__static_initialization_and_destruction_0ii
-	addl	$16, %esp
-	leave
-	.cfi_restore 5
-	.cfi_def_cfa 4, 4
-	ret
-	.cfi_endproc
-.LFE2225:
-	.size	_GLOBAL__sub_I_main, .-_GLOBAL__sub_I_main
-	.section	.init_array,"aw"
-	.align 4
-	.long	_GLOBAL__sub_I_main
-	.hidden	__dso_handle
-	.ident	"GCC: (GNU) 11.5.0 20240719 (Red Hat 11.5.0-5)"
-	.section	.note.GNU-stack,"",@progbits
+.section .data
+intro_double: .asciz "The double is:"
+.section .bss
+my_number: .skip 20
+my_char: .skip 1
+	.section .text
+	.global _start
+_start:
+	mov $0, %rax #0 for rax signifies a read operation
+	mov $0, %rdi #0 file descriptor for 
+	lea my_number(%rip), %rsi #stores the inputted number in buffer
+	mov $20, %rdx #store 20 bytes in the buffer
+	syscall #performs the operation
+
+	mov $0, %rbx #add 0 to rbx which is where we will store the number
+.ascii_to_int_start:
+	movb (%rsi), %al #moves only a byte of input to rax
+	cmpb $10, %al #stdin ends with linefeed character
+	je .ascii_to_int_end # compare to
+
+	movzx %al, %rax #moves the al to rax
+	#convert digit to number
+	imul $10, %rbx #multiply rbx by 10
+	add %rax, %rbx #adds the new digit to rax
+	sub $0x30, %rbx #subtract the 0 ascii
+
+	incq %rsi #move to next char 
+	jmp .ascii_to_int_start #return to loop start
+.ascii_to_int_end:
+	mov %rbx, %rax #mov rbx to rax since its open
+	imul $2, %rax #multiply rax by 2
+	push $0 #push to the stack, use as a sentinal
+.int_to_ascii_start:
+	cmp $0, %rax #loop ends when rax is 0
+	je .int_to_ascii_end # compare to
+
+	#divide by 10 to decrement and add remainder
+	xor %rdx, %rdx #clear the rdx register prior to division
+	mov $10, %rcx #make sure operand is same number of bytes
+	div %rcx #divides rax by 10
+	add $0x30, %rdx #adds the remainder byte to the remainder
+	push %rdx #push the rdx register to the stack
+
+	jmp .int_to_ascii_start #return to loop start
+.int_to_ascii_end:
+	mov $1, %rax #system call for sys_write
+	mov $1, %rdi #file desctiptor for stdout
+	lea intro_double(%rip), %rsi #loads the memory address
+	mov $15, %rdx #15 bytes is the length of the string
+	syscall
+
+	mov $1, %rdx #the length of the char will just be 1
+	lea my_char(%rip), %rsi #loads the memory address
+.print_number_start:
+	pop %rax #pop the char from the stack
+	cmp $0, %rax #compare the char to the 0 I previously added
+	je .print_number_end #end the loop
+
+	mov %al, (%rsi) #loads al into the value of my_char
+
+	mov $1, %rax #system call for sys_write
+	mov $1, %rdi #file desctiptor for stdout
+	syscall
+
+	jmp .print_number_start
+.print_number_end:
+
+	#print a linefeed to it doesn't look weird on gl
+	movb $10, (%rsi) #move linefeed to rsi
+	mov $1, %rax #system call for sys_write
+	mov $1, %rdi #file desctiptor for stdout
+	syscall
+
+	mov $60, %rax #syscall for exit
+	xor %rdi, %rdi #clears the rdi register giving exit status 0
+	syscall #exits the program
